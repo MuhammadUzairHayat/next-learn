6677f1bde0d218e03f4a4c2562c9e7c334975843
Muhammad Uzair <muhammad.druzair@gmail.com>
2025-02-13 11:15:45 +0500

    new commmit

---
+++ app/(root)/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        Invoices Page is here 
+    </div>
+  )
+}
+
+export default page
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,12 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { inter } from "@/app/ui/fonts";
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`flex flex-row items-center leading-none text-white gap-2`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <p className={`${inter.className} text-[44px] font-bold`}>Next.JS</p>
     </div>
   );
 }
